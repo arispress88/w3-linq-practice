3b73e23d8df51b7d0743651699ddf84e8d51417f
Arnold Rispress <arnold.rispress@gmail.com>
2023-06-01 14:38:51 -0400

    w3 linq practice #1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("The numbers which produce the remainder 0 after divided by 2 are:");
+
+        // Part 1 and 2... using Linq to filter numbers that produce a remainder of 0 when divided by 2.
+        var numbers = Enumerable.Range(0, 9).Where(num => num % 2 == 0);
+
+        // Part 3 prints filtered numbers
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
